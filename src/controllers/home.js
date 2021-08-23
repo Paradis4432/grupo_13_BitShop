5453714059ee6d060ee23d72c70ca8b9f9cce995
Laportam <84981059+Laportam@users.noreply.github.com>
2021-08-23 20:44:59 -0300

    asd
    asd

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,3 +1,30 @@
 module.exports = {
-    index: (req, res) => res.render("home", {style: "index", title: "BitShop"})
+    index: (req, res) => res.render("home", {style: "index", title: "BitShop"}),
+    compras: (req, res) => {
+        return res.render('compras');
+    },
+
+    historial: (req, res) => {
+        return res.render('historial');
+    },
+
+    favorites: (req, res) => {
+        return res.render('favorites');
+    },
+
+    name: (req, res) => {
+        return res.render('name');
+    },
+
+    email: (req, res) => {
+        return res.render('email');
+    },
+    
+    password: (req, res) => {
+        return res.render('password');
+    },
+
+    photo: (req, res) => {
+        return res.render('photo');
+    },
 }
--- src/routes/home.js
+++ src/routes/home.js
@@ -4,5 +4,18 @@ const home = require("../controllers/home");
 
 router.get("/", home.index);    
 
+router.get('/compras', home.compras);
+
+router.get('/historial', home.historial);
+
+router.get('/favorites', home.favorites);
+
+router.get('/config/name', home.name);
+
+router.get('/config/email', home.email);
+
+router.get('/config/password', home.password);
+
+router.get('/config/photo', home.photo);
 
 module.exports = router;
