870faff39f70f9fea649d314ac7ba0ee2bc794ad
Paradis4432 <Lucasezequiel120202@gmail.com>
2021-08-23 13:28:29 -0300

    Create user.js

---
+++ src/models/user.js
@@ -0,0 +1,11 @@
+const path = require('path')
+const fs = require('fs')
+
+const model = {
+    dir: path.resolve(__dirname, "../data", "users.json"),
+
+    allUsers: function(){
+        return JSON.parse(fs.readFileSync(this.dir))
+    },
+    
+}
