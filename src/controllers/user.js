94cbd5e0bcc93161975c5652040503a7ed8e1147
Paradis4432 <Lucasezequiel120202@gmail.com>
2021-08-23 14:57:11 -0300

    deatils

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,4 +1,26 @@
+const users = require("../models/user")
+
 module.exports = {
-    login: (req, res) => res.render("users/login", {style: "login", title: "User | Login"}),
-    register: (req, res) => res.render("users/register", {style: "register", title: "User | Register"})
+    login: (req, res) => res.render("users/login", { style: "login", title: "User | Login" }),
+    register: (req, res) => res.render("users/register", { style: "register", title: "User | Register" }),
+
+    createNewUser: (req, res) => {
+
+    },
+
+    checkIfUsersExists: (req, res) => {
+
+    },
+
+    removeUser: (req, res) => {
+
+    },
+
+    getUserPass: (req,res) => {
+
+    },
+
+    getUserEmail: (req,res) => {
+        
+    }
 }
--- src/models/user.js
+++ src/models/user.js
@@ -9,3 +9,5 @@ const model = {
     },
     
 }
+
+module.exports = model
